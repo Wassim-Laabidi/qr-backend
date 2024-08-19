f81801720f8ae081f72ad24a0531af052d107a7c
Wassim Laabidi <laabidi.wassim250@gmail.com>
2024-08-19 16:06:29 +0100

    add cors

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const { exec } = require('child_process');
+const cors = require('cors'); // Import the cors middleware
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
 app.use(bodyParser.json());
+app.use(cors()); // Enable CORS for all origins (not recommended for production) 
 
 app.post('/api/qr-code', (req, res) => {
     const { qrText } = req.body;
